388570bc70e5c6d206a06cbea6a6e6a56dd6404e
kadekdiantarii_2201010330 <119943301+diantariii@users.noreply.github.com>
2023-03-14 17:03:29 +0800

    SESI 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LATIHAN HTML SESI 1</title>
+</head>
+<body>
+    <h3>Belajar Web Programming<?="<sup>80</sup>"?></h3>
+    <P>Programming Web sangad azik</P>
+    <script>
+        document.write("<h3>Menggunakan Script javascript</h3>");
+    </script>
+
+<?php
+    echo "<h3>Menggunakan Script PHP</h3>";
+#echo setara dengan =
+?>
+
+</body>
+</html>
